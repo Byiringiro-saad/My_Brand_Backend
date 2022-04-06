924c3cc5df6ce0064c2ffeca2c4fb672a2e89ef1
Saad <byiringirosaad@gmail.com>
2022-04-06 20:22:14 +0200

    initial setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
+
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost:27017/portfolio", { useNewUrlParser: true })
+  .then(() => {
+    const app = express();
+    app.use(express.json());
+    app.use("/api", routes);
+
+    app.listen(5000, () => {
+      console.log("Server has started!");
+    });
+  });
