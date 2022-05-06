1a7c2e8e79b176ca3fffc1d11a78dcb675f85058
Saad <byiringirosaad@gmail.com>
2022-05-06 21:01:33 +0200

    make it better

--- services/db.service.js
+++ services/db.service.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 module.exports = mongoose
-  .connect("mongodb://localhost:27017/portfolio", { useNewUrlParser: true })
+  .connect(
+    "mongodb+srv://saad:hLoohkt0tfgRk5CZ@cluster0.c4fnv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
   .then(() => {
     console.log("database connected");
   });
